93ab8e95e6592b2d46020cee9968d11f33c9a023
billycode77 <billycode77@outlook.com>
2020-07-29 19:42:52 +0000

    initial commit

---
+++ spec/calcSpec.js
@@ -0,0 +1,8 @@
+describe("Calculator", function() {
+    describe("addition tests", function() {
+        it("should return 42", function() {
+            expect(adition(20,22)).toBe(42);
+        });
+    });
+});
+ 
